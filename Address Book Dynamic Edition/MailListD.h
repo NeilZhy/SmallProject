e745f1181b24d174767cf5a0c59aa8ba9e8d469e
NeilZhy <NeilZhy@163.com>
2017-02-19 22:58:10 +0800

    通讯录动态版

---
+++ Address Book Dynamic Edition/MailListD.h
@@ -0,0 +1,44 @@
+#ifndef __MAILLISTD_H__
+#define __MAILLISTD_H__
+
+#define NAME_MAX 10
+#define SEX_MAX 5
+#define TEL_MAX 12
+#define ADDRESS_MAX 15
+#define ALLMAX 2
+#define ADDMAX 3
+
+
+
+typedef struct PeoInfo
+{
+	char name[NAME_MAX];
+	char sex[SEX_MAX];
+	int age;
+	char tel[TEL_MAX];
+	char address[ADDRESS_MAX];
+}PeoInfo;
+
+typedef struct MailList
+{
+	//PeoInfo maillist[ALLMAX];
+	PeoInfo *maillist;
+	int count;
+	int MAX;
+}MailList, *pMailList;
+
+void menu();
+void test();
+void InitMailList(pMailList pmail);
+void add(pMailList pmail);
+void del(pMailList pmail);
+void Dprint(pMailList pmail, int i);
+int search(pMailList pmail);
+void del(pMailList pmail);
+void show(pMailList pmail);
+void empty(pMailList pmail);
+void change(pMailList pmail);
+void sort(pMailList pmail);
+
+
+#endif
